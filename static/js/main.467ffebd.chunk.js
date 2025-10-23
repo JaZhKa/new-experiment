e1feb426f4ed890f45c8fff3d1b1b526e485a954
Yakov <yakoff4@gmail.com>
2025-10-23 18:45:03 +0300

    first commit

---
+++ static/js/main.467ffebd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptokyo-white-react-admin-dashboard-js"]=this["webpackJsonptokyo-white-react-admin-dashboard-js"]||[]).push([[0],{9:function(t,e,a){"use strict";a.r(e);var n=a(3),r=a.n(n),o=a(0);var s=function(){return Object(o.jsx)("div",{children:"App React App"})};r.a.render(Object(o.jsx)(s,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.467ffebd.chunk.js.map
